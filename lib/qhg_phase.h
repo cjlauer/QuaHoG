7a79f50ef6ef8ac0d4228461c6aaffc40df4893a
Colin Lauer <colin.j.lauer@gmail.com>
2019-07-07 15:09:09 -0400

    Changed phase function name to be more general.

---
+++ lib/qhg_phase.h
@@ -0,0 +1,8 @@
+#ifndef _QHG_PHASE_H
+#define _QHG_PHASE_H 1
+
+#include <qhg_types.h>
+
+void qhg_phase(qhg_spinor_field [], qhg_spinor_field [], int mom[3], int source_coords[4], double);
+
+#endif /* _QHG_PHASE_H */
